293a817cedd33fc46f00dd9a09012c9490e88bde
lujun9972 <lujun9972@gmail.com>
2016-02-12 19:06:57 +0800

    script used to generate README.org

---
+++ make-readme.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+TITLE="emacs lisp faq"
+
+echo $TITLE >README.org
+echo >> README.org
+
+for file in $(ls *.org |grep -v README.org); do
+    echo "[[file:$file][$file]]" >>README.org
+done
